1d4abf420604635650e5e008a6dc5622e63c182a
Zachariah Olenik <zjolenik139@gmail.com>
2022-08-29 10:50:49 -0400

    done

---
+++ PracticeLinq/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PracticeLinq
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var list = new List<string>()             
+            { 
+                "Fifa",
+                "another video game", 
+                "video game" 
+            };
+
+            list.OrderBy(x => x.Length).ToList().ForEach(x => Console.WriteLine(x));
+        }
+    }
+}
